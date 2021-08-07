0e5d76bcc8f3f03ee72d8b2cb5ff67c40b34c1a9
carlandres18@gmail.com <carlandres18@gmail.com>
2021-08-07 15:30:07 +0800

    Main Window Closable

---
+++ Desk.Aesthetics.PinnedImages.Presentation/Commands/CloseWindowCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Desk.Aesthetics.PinnedImages.Presentation.Commands
+{
+    public class CloseWindowCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (typeof(Window).IsAssignableFrom(parameter.GetType())) (parameter as Window).Close();
+        }
+    }
+}
