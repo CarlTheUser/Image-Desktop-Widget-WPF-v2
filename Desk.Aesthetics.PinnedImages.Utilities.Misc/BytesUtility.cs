e2a4a92c8ca5b163f3e9de3b12ca9979183105c4
carlandres18@gmail.com <carlandres18@gmail.com>
2021-08-21 16:36:10 +0800

    Added Utilities Project

---
+++ Desk.Aesthetics.PinnedImages.Utilities.Misc/BytesUtility.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Desk.Aesthetics.PinnedImages.Utilities.Misc
+{
+    public static class BytesUtility
+    {
+        public static string FormatBytesString(this byte[] bytes)
+        {
+            StringBuilder guidBinary = new StringBuilder();
+            foreach (byte guidByte in bytes)
+            {
+                guidBinary.AppendFormat(@"{0}", guidByte.ToString("x2"));
+            }
+
+            return guidBinary.ToString();
+        }
+    }
+}
